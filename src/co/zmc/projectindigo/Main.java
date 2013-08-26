ad6412199f17e2dd45c34a75e2f07627120ef006
ZephyrusMC <vanZeben@live.ca>
2013-08-26 17:22:47 -0400

    Why not just send the JSON directly though here, instead of going to another URL

--- src/co/zmc/projectindigo/Main.java
+++ src/co/zmc/projectindigo/Main.java
@@ -13,10 +13,40 @@ import cpw.mods.fml.common.network.NetworkMod;
 public class Main {
     @Mod.Instance
     public static Main _instance;
-    private String     _serverInformationURL;
+    private String     _serverName;
+    private String     _serverIP;
+    private String     _logoURL;
+    private String     _packHost;
+    private String     _packName;
+    private String     _packLocation;
+    private String     _packVersion;
 
-    public String getServerInformationURL() {
-        return _serverInformationURL;
+    public String getServerName() {
+        return _serverName;
+    }
+
+    public String getServerIP() {
+        return _serverIP;
+    }
+
+    public String getLogoURL() {
+        return _logoURL;
+    }
+
+    public String getPackHost() {
+        return _packHost;
+    }
+
+    public String getPackName() {
+        return _packName;
+    }
+
+    public String getPackLocation() {
+        return _packLocation;
+    }
+
+    public String getPackVersion() {
+        return _packVersion;
     }
 
     @Mod.PreInit
@@ -25,9 +55,16 @@ public class Main {
 
         Configuration config = new Configuration(event.getSuggestedConfigurationFile());
         config.load();
-        String GENERAL = Configuration.CATEGORY_GENERAL;
-        _serverInformationURL = config.get(GENERAL, "server_information_url", "http://example.com/server_info.json",
-                "Link to the server's JSON information page").getString();
+        _serverName = config.get("Server", "name", "Example's Server", "Localized name for your server").getString();
+        _serverIP = config.get("Server", "ip", "example.com", "URL/IP for your server, that you want users to connect to")
+                .getString();
+        _logoURL = config.get("Server", "logo", "http://example.com/example.png",
+                "Image you want to represent your server (Max size: 250px X 250px)").getString();
+        _packHost = config.get("Modpack", "host", "FTB", "The type of server this is (options: FTB/Technic/Custom)").getString();
+        _packName = config.get("Modpack", "name", "Unleashed", "Name of the modpack (FTB modpacks: (Unleashed)").getString();
+        _packLocation = config.get("Modpack", "url", "http://example.com/modpack.zip",
+                "Link to download the modpack (only needed for custom packs)").getString();
+        _packVersion = config.get("Modpack", "version", "1.0.0", "Version of the modpack").getString();
         config.save();
     }
 
--- src/co/zmc/projectindigo/PacketHandler.java
+++ src/co/zmc/projectindigo/PacketHandler.java
@@ -7,15 +7,25 @@ import cpw.mods.fml.common.network.Player;
 
 public class PacketHandler implements IPacketHandler {
 
+    private String getData() {
+        String data = "{ \"name\": \"" + Main._instance.getServerName() + "\",";
+        data += "\"ip\": \"" + Main._instance.getServerIP() + "\",";
+        data += "\"logo\": \"" + Main._instance.getLogoURL() + "\",";
+        data += "\"modpack_host\": \"" + Main._instance.getPackHost() + "\",";
+        data += "\"modpack_name\": \"" + Main._instance.getPackName() + "\",";
+        data += "\"modpack_loc\": \"" + Main._instance.getPackLocation() + "\",";
+        data += "\"modpack_version\": \"" + Main._instance.getPackVersion() + "\" }";
+        return data;
+    }
+
     @Override
     public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player) {
         if (packet.channel.equals("projectindigo") && new String(packet.data).equalsIgnoreCase("request_modpack_url")) {
             Packet250CustomPayload newPacket = new Packet250CustomPayload();
             newPacket.channel = "projectindigo";
-            newPacket.length = Main._instance.getServerInformationURL().trim().getBytes().length;
-            newPacket.data = Main._instance.getServerInformationURL().trim().getBytes();
+            newPacket.length = getData().trim().getBytes().length;
+            newPacket.data = getData().trim().getBytes();
             manager.addToSendQueue(newPacket);
-            System.out.println("Packet requested and returned");
         }
     }
 
